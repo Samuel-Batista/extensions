4668ae55a09247d5e904a57e8f045d3ce1f2c49a
Samuel-Batista <84580161+Samuel-Batista@users.noreply.github.com>
2022-06-21 13:44:16 -0300

    add js

---
+++ extension/main.js
@@ -0,0 +1,11 @@
+// var elements = document.querySelectorAll('p')
+
+// var chance = 100
+
+// for (i=0 ; elements.length - 1; i++) {
+//     var number = Math.floor(Math.random() * 101);
+
+//     if (number <= chance) {
+//         elements[i].innerHTML = ''
+//     }
+// };
