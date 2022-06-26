954109158ffdab11e48eb325ae3b3f825967f3b2
Samuel-Batista <84580161+Samuel-Batista@users.noreply.github.com>
2022-06-26 16:23:19 -0300

    v2

--- extension/main.js
+++ extension/main.js
@@ -1,21 +1,25 @@
-var elements = document.querySelectorAll('img');
+function spawnTheRock() {
+    let elements = document.querySelectorAll('img');
+    let elementsAmmount = elements.length;
+    let ramdomNumber = Math.floor(Math.random() * elementsAmmount);
+    let elementoTheRock  = elements[ramdomNumber];
 
-var rocks = [
-    "https://c.tenor.com/MThz30WrMWQAAAAd/the-rock-chinese-the-rock.gif",
-    "https://c.tenor.com/ACQn5n-60vcAAAAC/xd-iksdi.gif",
-    "https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif",
-    "https://c.tenor.com/qCECg-F0y90AAAAC/the-rock-dwayne-johnson.gif",
-    "https://c.tenor.com/RP_qoKH85xgAAAAd/the-rock-sus-the-rock-meme.gif",
-    "https://c.tenor.com/CoH0pdIdiEEAAAAC/therock.gif",
-    "https://c.tenor.com/GkY0BS3F0R4AAAAd/the-rock-just-bring-it.gif",
-];
-
-var chance = 1;
-
-for (i=0 ; elements.length - 1; i++) {
-    var number = Math.floor(Math.random() * 201);
-
-    if (number <= chance) {
-        elements[i].src = rocks[Math.floor(Math.random() * 6)];
-    }
+    elementoTheRock.classList.add(`tr${Math.floor(Math.random() * 6)}`)
 };
+
+function killTheRock() {
+    for (let i=1; i<8; i++) {
+        let supostoTheRock = document.querySelector(`.tr${i}`);
+        if (supostoTheRock !== null) {
+            supostoTheRock.classList.remove(`tr${i}`)
+        };
+    };
+}
+
+function main() {
+    spawnTheRock();
+    setTimeout(killTheRock, 500);
+};
+
+
+setInterval(main, 1000);
